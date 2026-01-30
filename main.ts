2e29ec7bd6966717ce6e9b12a76856578908e973
Porawit Poboonma <ball6847@gmail.com>
2026-01-30 08:42:49 +0700

    feat: add workspace manager configuration and documentation

--- main.ts
+++ main.ts
@@ -6,6 +6,14 @@ if (!UPSTREAM) {
   Deno.exit(1);
 }
 
+// TODO: make proxy work at /proxy/:path paths (so we have slot for adding features)
+// TODO: add /api/kill-switch/ endpoint to quickly manage proxy kill switch (force status, headers and response body)
+// TODO: add /api/delay/ endpoint to manage proxy delay
+// TODO: add github action to Deno Deploy (might not need to pre configured at our end, could be done seamlessly at Deno Deploy)
+// TODO: add json structured logging for request / response (need to find a good json logging library / try `pino` if it compatible with deno)
+// TODO: use Deno KV as datasource (use on-cloud option at Deno Deploy free plan)
+// TODO: keep everything in this file main.ts to keep it simple
+
 console.log(`Starting proxy server`);
 console.log(`Upstream: ${UPSTREAM}`);
 console.log(`Delay: ${DELAY}ms`);
